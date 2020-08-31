7f9555abb55dc2e088d76ba31a020310cbc991d1
xiaochi <cumt.xiaochi@gmail.com>
2020-08-31 14:18:57 +0800

    sql

--- chat.sql
+++ chat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2020 at 04:10 PM
+-- Generation Time: Aug 31, 2020 at 08:18 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -30,26 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `chat` (
   `id` bigint(20) NOT NULL,
+  `uid` int(11) NOT NULL,
   `username` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `chat`
---
-
-INSERT INTO `chat` (`id`, `username`, `content`, `created`) VALUES
-(1, 'U2FsdGVkX1/35RC/DObLBx', '0', '2020-08-22 22:02:29'),
-(2, 'U2FsdGVkX19x5Lu4OLTfcB', '0', '2020-08-22 22:04:06'),
-(3, 'U2FsdGVkX18+KlzPlYnCJk', 'U2FsdGVkX1917s0cVDF/pKtIYX2c4kG9OZ2glj65QYQ=', '2020-08-22 22:04:56'),
-(4, 'U2FsdGVkX1+WYlNLjcnCxF', 'U2FsdGVkX1/XyT2e/3nth8soL9wWngU3oWq1x9adc9I=', '2020-08-22 22:05:02'),
-(5, 'U2FsdGVkX19prk3ndXGtpK', 'U2FsdGVkX1/mKJZQQCu4gmFA26WZfSu7aha+X6vWCew=', '2020-08-22 22:05:55'),
-(6, 'U2FsdGVkX1/7Lx9R9grfWH', 'U2FsdGVkX1+xaDKDq4B8SSxgRJzfKb+GFMMYy/RCHlk=', '2020-08-22 22:07:42'),
-(7, 'U2FsdGVkX1930SypmKv9JT', 'U2FsdGVkX1/qCe7VWm3EbiMokSJUwG8Hd7Iq/KARniU=', '2020-08-22 22:08:06'),
-(8, 'U2FsdGVkX199U6nSxEDFdv', 'U2FsdGVkX1+oN8yM+f+mGJzy3VWGYVJKWy9eiZseqXg=', '2020-08-22 22:09:05'),
-(9, 'U2FsdGVkX1+R279F+/UvLvihSJKdiQW+/MvLYxLpu6o=', 'U2FsdGVkX18foDeDHSOzRlPsiNhtNw0oLqEsfPbmp4M=', '2020-08-22 22:09:31'),
-(10, 'U2FsdGVkX1+z+gBXhiY8i9vuoz2ucIR4VwHYXdIcMmo=', 'U2FsdGVkX1/A+jSLwbeZTDFJ6Gt2g4ONLeXq/uK4kuU=', '2020-08-22 22:09:33');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +54,7 @@ ALTER TABLE `chat`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
